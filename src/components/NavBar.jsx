363079f6907313e718668fad7ef88c321aabc591
abrusu-collins <snillochemingson@gmail.com>
2023-06-24 17:45:26 +0000

    navbar done

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,65 @@
+import React, { useState } from "react";
+import { HiBars3 } from "react-icons/hi2";
+import { TfiClose } from "react-icons/tfi";
+import { MdDriveFileRenameOutline } from "react-icons/md";
+import Link from "next/link";
+function NavBar() {
+  const [navOpen, setNavOpen] = useState(false);
+  return (
+    <nav>
+      <div className="navbar">
+        <Link href="/">
+          <MdDriveFileRenameOutline size={35}  color="white"/>
+        </Link>
+        <ul>
+          <li>
+            <Link href="/">Home</Link>
+          </li>
+          <li>
+            <Link href="/single">Single Image</Link>
+          </li>
+          <li>
+            <Link href="/multiple">Multiple Images</Link>
+          </li>
+          <li>
+            <Link href="sponsor">Sponsor</Link>
+          </li>
+        </ul>
+        <div className="bar">
+          {navOpen ? (
+            <TfiClose
+              color="white"
+              size={30}
+              onClick={(e) => {
+                setNavOpen(!navOpen);
+              }}
+            />
+          ) : (
+            <HiBars3
+              size={35}
+              onClick={(e) => {
+                setNavOpen(!navOpen);
+              }}
+            />
+          )}
+        </div>
+      </div>
+      <ul className={navOpen ? "mobile-nav" : ""}>
+        <li>
+          <Link href="/">Home</Link>
+        </li>
+        <li>
+          <Link href="/single">Single Image</Link>
+        </li>
+        <li>
+          <Link href="/multiple">Multiple Images</Link>
+        </li>
+        <li>
+          <Link href="/sponsor">Sponsor</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default NavBar;
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,10 @@
-
 import "../styles/index.css";
+import NavBar from "@/components/NavBar";
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <NavBar />
+      <Component {...pageProps} />
+    </>
+  );
 }
