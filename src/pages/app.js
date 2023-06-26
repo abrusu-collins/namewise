f7f460800c1b36ad002cf9c8176d11d007a13f83
abrusu-collins <snillochemingson@gmail.com>
2023-06-26 00:34:53 +0000

    done

--- src/pages/app.js
+++ src/pages/app.js
@@ -1,4 +1,6 @@
 import React, { useEffect, useState } from "react";
+import nProgress, * as NProgress from "nprogress";
+import "nprogress/nprogress.css";
 import { Configuration, OpenAIApi } from "openai";
 const configuration = new Configuration({
   apiKey: process.env.NEXT_PUBLIC_OPENAI_API_KEY,
@@ -12,7 +14,6 @@ function Generator() {
   const [emptyIndustry, setEmptyIndustry] = useState(false);
   const [emptyWork, setEmptyWork] = useState(false);
   const [resultArray, setResultArray] = useState([]);
-
   const generate = (e) => {
     e.preventDefault();
     if (!industry) {
@@ -23,6 +24,8 @@ function Generator() {
       setEmptyWork(true);
       return;
     }
+    NProgress.start();
+    // setResultArray([]);
     openai
       .createCompletion({
         model: "text-davinci-003",
@@ -38,10 +41,12 @@ function Generator() {
         return res;
       })
       .then((data) => {
+        NProgress.done();
         setResultArray(data?.data?.choices[0]?.text.split(","));
         console.log(data.data.choices[0].text);
       })
       .catch((err) => {
+        NProgress.done();
         console.log(err);
       });
   };
@@ -90,7 +95,7 @@ function Generator() {
       </div>
       {resultArray.length > 0 && (
         <p className="results-title">
-          These are the {numberOfResults} names you asked for
+          These are the {numberOfResults} names you asked for😍
         </p>
       )}
       <div className="results">
@@ -99,6 +104,11 @@ function Generator() {
             return <p key={names}>{names}</p>;
           })}
       </div>
+      {resultArray.length > 0 && (
+        <a href=" " className="regenerate" onClick={generate}>
+          Regenerate
+        </a>
+      )}
     </div>
   );
 }
